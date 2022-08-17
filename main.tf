703d63dd2c9b219e241d8a323745cb7b57d33ed3
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-17 10:53:15 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+
+variable "reponame" {}
+variable "container_port" {}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = var.reponame   
+  ports {
+    internal = 80
+    external = var.container_port 
+  }
+}
+
+
+// en terraformm para declarar variables se usa la palabra "variable" sin embargo para utilizarlas se utiliza el prefijo "var.(nombre de la variable)".
